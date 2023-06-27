ea04dbf293bfbc8cd2850dd3bfec870b51b09cbb
dawnzs01 <18013865675@163.com>
2023-06-27 14:22:42 +0800

    iris

---
+++ src/main/java/net/coderbot/batchedentityrendering/impl/BufferBuilderExt.java
@@ -0,0 +1,9 @@
+package net.coderbot.batchedentityrendering.impl;
+
+import com.mojang.blaze3d.vertex.BufferBuilder;
+
+import java.nio.ByteBuffer;
+
+public interface BufferBuilderExt {
+    void splitStrip();
+}
